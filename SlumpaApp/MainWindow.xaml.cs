28400ffaa6d2bfee79f80b3a2bd0f5dcaeaf70bb
Dante Wallén <dante.wallen@elev.ntig.se>
2025-02-12 15:02:46 +0100

    tränat på xaml & tryparse

---
+++ SlumpaApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlumpaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SlumpaTal(object sender, RoutedEventArgs e)
+    {
+        if (!int.TryParse(Mingräns.Text, out int mintal))
+        {
+            SlumptalOutput.Text = "Felaktig inmatning".ToString();
+            return;
+        }
+
+        if (!int.TryParse(Maxgräns.Text, out int maxtal))
+        {
+            SlumptalOutput.Text = "Felaktig inmatning".ToString();
+            return;
+        }
+
+        int slumptal = Random.Shared.Next(mintal, maxtal + 1);
+        SlumptalOutput.Text = slumptal.ToString();
+    }
+}
