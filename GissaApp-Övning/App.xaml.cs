32e34fbd8c38d1b12d30c4baa5820085c10247ff
Dante Wallén <dante.wallen@elev.ntig.se>
2025-04-07 12:56:18 +0200

    övningsprov - övat själv

---
+++ GissaApp-Övning/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GissaApp_Övning;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ GissaApp-Övning/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GissaApp_Övning;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int random = Random.Shared.Next(1, 101);
+    List<int> listaGissningar = [];
+
+    public void klickGissa(object sender, RoutedEventArgs e)
+    {
+        string inputText = txbInput.Text;
+        bool success = int.TryParse(inputText, out int inputGissning);
+
+        if (success)
+        {
+
+            listaGissningar.Add(inputGissning);
+
+            if (inputGissning == random)
+            {
+                txbResultat.Text = "Din gissning var rätt";
+            }
+            else if (inputGissning <= random)
+            {
+                txbResultat.Text = $"Det rätta svaret är större än {inputGissning}";
+            }
+            else if (inputGissning >= random)
+            {
+                txbResultat.Text = $"Det rätta svaret är mindre än {inputGissning}";
+            }
+        }
+        else
+        {
+            txbResultat.Text = "Något blev fel med inmatningen.";
+        }
+    }
+
+    private void klickVisaFacit(object sender, RoutedEventArgs e)
+    {
+        txbResultat.Text = $"Det rätta svaret var {random}";
+    }
+
+    private void klickHistorik(object sender, RoutedEventArgs e)
+    {
+        txbHistorik.Text = "";
+
+        foreach (var item in listaGissningar)
+        {
+            txbHistorik.Text += $"{item}\n";
+        }
+    }
+
+    public void klickSpelaIgen(object sender, RoutedEventArgs e)
+    {
+        random = Random.Shared.Next(1, 101);
+        listaGissningar = [];
+
+        txbResultat.Text = "";
+        txbInput.Text = "";
+        txbHistorik.Text = "";
+    }
+}
